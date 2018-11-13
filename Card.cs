9bf672f8a81838669e168aead24e27438e0fccec
Ben Jen <benjendrzejak@quickenloans.com>
2018-11-12 21:38:54 -0500

    Fixed syntax

--- Card.cs
+++ Card.cs
@@ -1,44 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercise60
 {
-    public enum Suit { Spades = 1, Diamonds, Hearts, Clubs }
-    public enum Rank { Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King }
-    class Card
+    public enum Suit
     {
+        Spades = 1,
+        Diamonds,
+        Hearts,
+        Clubs
+    }
+
+    public enum Rank
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    internal class Card
+    {
+        private static List<Card> _deckOfCards = new List<Card>(52);
         public Suit Suit { get; set; }
         public Rank Rank { get; set; }
 
-        static List<Card> _deckOfCards = new List<Card>(52);
-
         public static void CreateDeck()
         {
-            _deckOfCards = Enumerable.Range(1, 4)
-                .SelectMany(s => Enumerable.Range(1, 13)
-                    .Select(c => new Card()
-                        {
-                            Suit = (Suit)s,
-                            Rank = (Rank)c
-                        }
-                    )
-                )
-                .ToList();
+            _deckOfCards = Enumerable.Range(1, 4).SelectMany(s => Enumerable.Range(1, 13).Select(r => new Card {Suit = (Suit) s, Rank = (Rank) r})).ToList();
 
-            _deckOfCards = _deckOfCards.OrderBy(c => Guid.NewGuid()).ToList();
+
         }
 
-        public static Card DisplayCard()
+        public static Card ShuffleCards()
         {
+            _deckOfCards = _deckOfCards.OrderBy(c => Guid.NewGuid()).ToList();
 
             var card = _deckOfCards.FirstOrDefault();
             _deckOfCards.Remove(card);
 
             return card;
         }
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,25 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace Exercise60
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             do
             {
                 Card.CreateDeck();
-                Card card = Card.DisplayCard();
+                var card = Card.ShuffleCards();
 
-                Console.WriteLine($"Shuffling...you drew a {card.Rank} of {card.Suit}.");
+                WriteLine($"Shuffling...you drew a {card.Rank} of {card.Suit}.");
 
-                Console.Write("Would you like to continue? (y/n): ");
-            } while (Console.ReadLine().Equals("y", StringComparison.OrdinalIgnoreCase));
-
+                Write("Would you like to continue? (y/n): ");
+            } while (ReadLine().Equals("y", StringComparison.OrdinalIgnoreCase));
         }
     }
 }
