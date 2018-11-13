3ee3e7cd2aad5501ca67a2d5122871ba121ca66c
Ben Jen <benjendrzejak@quickenloans.com>
2018-11-12 21:28:45 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Card.CreateDeck();
+                Card card = Card.DisplayCard();
+
+                Console.WriteLine($"Shuffling...you drew a {card.Rank} of {card.Suit}.");
+
+                Console.Write("Would you like to continue? (y/n): ");
+            } while (Console.ReadLine().Equals("y", StringComparison.OrdinalIgnoreCase));
+
+        }
+    }
+}
